56a9113599e768a0f304916ef4f7db5cbc251365
23rc <23rcroberto@gmail.com>
2024-07-19 03:03:11 -0600

    roberto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit  } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -6,9 +6,18 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css','./app.portada.component.css','./app.sobreMi.component.css','./app.proyectos.component.css','./app.contacto.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Portafolio_Digital';
+  ngAfterViewInit() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
 
+    if (menuToggle && navLinks) {
+      menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+      });
+    }
+  }
   mostrarGaleria: { [key: number]: boolean } = {};
   imagenSeleccionada: string | null = null;
 
